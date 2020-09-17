5290ab92b8e4c7661c2e4b68973239a9ffa0c7ce
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-17 09:26:33 -0400

    Très beau Travail
    Le code est bien commenté et la présentation est belle !

--- Projet-Exercice1/PairImpair.cpp
+++ Projet-Exercice1/PairImpair.cpp
@@ -1,15 +1,15 @@
-// But : Écrire un programme qui lit un entier et qui affiche si le nombre est pair ou impair
+// But : Ã‰crire un programme qui lit un entier et qui affiche si le nombre est pair ou impair
 // Auteur : Arnaud Ouimet
 // Date : 2020-09-13
 
-#include <iostream>    // Pour que le programme puisse intéragir avec L'utilisateur cout, cin
+#include <iostream>    // Pour que le programme puisse intÃ©ragir avec L'utilisateur cout, cin
 using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "");           //Permet de convertir les accents dans la console
 
-	// Déclaration des variables nécessaire au programme
+	// DÃ©claration des variables nÃ©cessaire au programme
 	int entier;
 
 
@@ -18,18 +18,19 @@ int main()
 	cin >> entier;
 	
 
-	// Le programme indique si les valeurs sont pair ou impair
+	// FR : Le programme indique si les valeurs sont pair ou impair
 
 	if (entier % 2 == 0) // Si l'entier est divisible par 2, alors l'entier = pair
 	{
 		cout << entier << " est un nombre pair \n";
 	}
-
-	else if (entier != 0) // Si l'entier n'égale pas la valeur d'en haut, alors l'entier = impair
+	// KM : Pas besoin de refaire un test ici car %2 donne deux rÃ©ponses possibles seulement (0 et 1) et surtout il manque le % 2
+	else if (entier != 0) // Si l'entier n'Ã©gale pas la valeur d'en haut, alors l'entier = impair
 	{
 	    cout << entier << " est un nombre impair \n";
 	}
-	// Pour repousser le texte Windows qui s'affiche à la fin ( Ça fait plus beau)
+	// Pour repousser le texte Windows qui s'affiche Ã  la fin ( Ã‡a fait plus beau)
+	// KM : Effectivement !!!
 	{
 		cout << "\n";
 		cout << "\n";
@@ -49,9 +50,10 @@ int main()
 		cout << "\n";
 	}
 
-	// Le plan du test
+	// Le plan du test 
+	// KM : Teste aussi des valeurs nÃ©gatives et 0 !!
 	/*
-	Entier           Résultat
+	Entier           RÃ©sultat
 
 	  32            32 est un nombre pair
 	  23            23 est un nombre impair
